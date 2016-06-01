3a459273e0d7c457ff357914c02c779a9975f190
zerto <zerto@LAPTOP-5THLSOAA>
2016-05-31 19:13:17 -0700

    Just getting updates

--- src/byui/cit260/jurassicpark/control/MapControl.java
+++ src/byui/cit260/jurassicpark/control/MapControl.java
@@ -10,5 +10,8 @@ package byui.cit260.jurassicpark.control;
  * @author Matthew
  */
 public class MapControl {
-    
+    public double calcResistance (double voltage, double amps){
+        return voltage/amps;
+        
+    }
 }
