7e12147deb5c298e4ec383023b1ecdcdb055d56f
Tim <zerto@LAPTOP-5THLSOAA>
2016-07-07 21:55:49 -0700

    Small Change

--- src/byui/cit260/jurassicpark/view/GameMainView.java
+++ src/byui/cit260/jurassicpark/view/GameMainView.java
@@ -13,8 +13,8 @@ import java.util.Scanner;
 import jurassicpark.JurassicPark;
 
 /**
- * This File needs to be redone. This is the in game menu not the main menu.
- * @author Matthew
+ * 
+ * @author Tim W
  */
 
 public class GameMainView extends View {
@@ -94,34 +94,7 @@ public class GameMainView extends View {
                 "\n Your locations is " + playerLocation.getRow() + ", " + playerLocation.getCol()
         );
     }
-    
-    
-    private void describeGame() {
-        System.out.println("\n--------------------------------------------" +
-        "\n- You are a guest at a new park featuring  -" +
-        "\n- dinosaurs. But something happened and    -" +
-        "\n- now there are hungry carnivore out and   -" +
-        "\n- very hungry!                             -" +
-        "\n--------------------------------------------");
-        
-    }
-
-    private void describePlayer() {
-         System.out.println("CALLED START NEW GAME - NIY");
-    }
-
-    private void describeRaptop() {
-         System.out.println("CALLED START NEW GAME - NIY");
-    }
-    private void describeTrex() {
-         System.out.println();
-    }
-    private void playGame() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    private void describeRaptor() {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
+       
 }
 
     
