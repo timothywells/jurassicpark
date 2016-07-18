3b1d84b262c9f6fe715666a7d94c5eb6d18f7da6
christian <christian@192.168.0.29>
2016-07-17 22:19:53 -0600

    mapControlException, WinGameException, helpMenuView

---
+++ src/byui/cit260/jurassicpark/exception/mapControlException.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.jurassicpark.exception;
+
+/**
+ *
+ * @author christian
+ */
+public class mapControlException extends Exception{
+
+    public mapControlException() {
+    }
+
+    public mapControlException(String message) {
+        super(message);
+    }
+
+    public mapControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public mapControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public mapControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    
+}
